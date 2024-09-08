8b708e6476d5bf090ed33470b1f16c283709f757
Sillas Emanoel <jedi116@hotmail.com>
2024-09-08 18:56:35 -0300

    fixing login client redirect error

--- app/(auth)/login-client/page.tsx
+++ app/(auth)/login-client/page.tsx
@@ -3,17 +3,14 @@
 import { useSession } from "next-auth/react";
 import LoginForm from "./_components/login-form";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function LoginClientPage() {
   const router = useRouter();
   const { status } = useSession();
 
-  useEffect(() => {
-    if (status === "authenticated") {
-      router.push("/dashboard");
-    }
-  }, [status, router]);
+  if (status === "authenticated") {
+    router.push("/dashboard");
+  }
 
   return <LoginForm />;
 }
